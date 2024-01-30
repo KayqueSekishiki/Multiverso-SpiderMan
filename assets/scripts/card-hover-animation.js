c21fce3bd5e050ff0e6a029312b5131add5d74cc
Kayque Lopes <104032451+KayqueSekishiki@users.noreply.github.com>
2024-01-30 01:57:25 -0300

    adicionando animações no body

--- assets/scripts/card-hover-animation.js
+++ assets/scripts/card-hover-animation.js
@@ -10,10 +10,12 @@ function addEventListernersToCards() {
 
 function handleMouseEnter() {
   this.classList.add("s-card--hovered");
+  document.body.id = `${this.id}-hovered`;
 }
 
 function handleMouseLeave() {
   this.classList.remove("s-card--hovered");
+  document.body.id = ``;
 }
 
 document.addEventListener("DOMContentLoaded", addEventListernersToCards, false);
